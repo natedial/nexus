4104f5b9316614a1f1e428489d0cd6661494cfe9
natedial <nathaniel.c.dial@gmail.com>
2026-03-15 22:26:31 -0400

    research-store: feat: add tool schema and export search/corpus_info from package

--- packages/research-store/distill_tool/__init__.py
+++ packages/research-store/distill_tool/__init__.py
@@ -1,4 +1,5 @@
 from distill_tool.pipeline import distill_file, distill_markdown
 from distill_tool.search import HybridSearchEngine
+from distill_tool.api import search, corpus_info
 
-__all__ = ["distill_file", "distill_markdown", "HybridSearchEngine"]
+__all__ = ["distill_file", "distill_markdown", "HybridSearchEngine", "search", "corpus_info"]
